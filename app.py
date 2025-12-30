d72b7acd20950faf9ba6fb2afe6607135f4b26c5
Lijin Kurian <lijin.kurian@gmail.com>
2025-12-30 12:15:30 +0530

    Run app in production mode on 0.0.0.0
    Change app to run in production mode on all interfaces.

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
